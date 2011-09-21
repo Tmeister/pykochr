4166f5a842366be3904f2be991636f3ad018f3b7
Enrique Chavez <noone@tmeister.net>
2011-09-20 20:45:46 -0500

    Old IE messages

---
+++ static/js/en_US.js
@@ -0,0 +1,21 @@
+//render this via backend
+window.IE6WarningLocalizations ={
+    msg1: "Did you know that your Internet Explorer is out of date?",
+    msg2: "To get the best possible experience using our website we recommend that you upgrade to a newer version or other web browser. A list of the most popular web browsers can be found below.",
+    msg3: "Just click on the icons to get to the download page",
+    br: {
+        ie: "Internet Explorer 9+",
+        ff: "Firefox 4+",
+        safari: "Safari 5+",
+        opera: "Opera 11+",
+        chrome: "Chrome 10.0+"
+    },
+    url: {
+        ie: "http://www.microsoft.com/windows/Internet-explorer/default.aspx",
+        ff: "http://www.mozilla.com/firefox/",
+        safari: "http://www.apple.com/safari/download/",
+        opera: "http://www.opera.com/download/",
+        chrome: "http://www.google.com/chrome"
+    }
+};
+
